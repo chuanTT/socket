5b0cb16270397beccc9a8fd0bc40e66d02698f28
chuanTT <chuan123789@gmail.com>
2025-01-23 16:49:27 +0700

    update

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 // Khởi tạo ứng dụng Express và HTTP server
 const app = express();
 const server = http.createServer(app);
 
 // Tạo Socket.io server
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "*", // Cho phép tất cả các origin kết nối
+  },
+});
 
 // Kết nối tới client
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   // Lắng nghe sự kiện từ client
-  socket.on('sync_data', (data) => {
+  socket.on("sync_data", (data) => {
     // Gửi lại dữ liệu cho tất cả client
-    io.emit('sync_data', data);
+    io.emit("sync_data", data);
   });
 });
 
 // Chạy server trên cổng 3000
 server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
