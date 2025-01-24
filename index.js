61ecac35327aa905fefa47b59ee014f780113877
Nguyễn Đình Chuân <95389918+chuanTT@users.noreply.github.com>
2025-01-24 10:43:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-require('dotenv').config()
+
 
 // Khởi tạo ứng dụng Express và HTTP server
 const app = express();
@@ -22,7 +22,7 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("sync_data", data);
   });
 });
-const port = process.env.PORT || 5000
+const port = 5000
 console.log("🚀 ~ port:", port)
 // Chạy server trên cổng 3000
 server.listen( port, () => {
